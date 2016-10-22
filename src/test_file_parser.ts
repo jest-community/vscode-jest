0bab4fb69f4621ee50a22045d20a4e019e67e315
Orta Therox <orta.therox@gmail.com>
2016-10-22 09:43:18 +0100

    Use the babylon parser to pull out the it blocks from a test file

--- src/test_file_parser.ts
+++ src/test_file_parser.ts
@@ -5,23 +5,86 @@ import fs = require('fs');
 import {basename, dirname} from 'path';
 import * as path from 'path';
 
-// var esprima = require('./esprima.js');
-// import esprima from 'esprima'
-var esprima = require('esprima');
+// var esprima = require('esprima');
+import * as babylon from 'babylon'
 
-// var esprima = require("src/esprima.ts")
+interface Location {
+    line: number
+    column: number
+}
 
+export class ItBlock {
+    name: string
+    file: string
+    start: Location
+    end: Location
+
+    updateWithNode(node: any){
+        this.start = node.loc.start
+        this.end = node.loc.end
+        this.name = node.expression.arguments[0].value
+    }
+}
 
 export default class TestFileParser {
-    run(file: string): Promise<any> {
-        return new Promise((resolve, reject) =>{
-            console.log( process.cwd() )
 
+    itBlocks: ItBlock[]
+
+    async run(file: string): Promise<any> {
+        let data = await this.generateAST(file)
+        this.itBlocks = []
+        this.findItBlocksInBody(data["program"])
+        return data
+    }
+
+    foundItNode(node){
+        let it = new ItBlock()
+        it.updateWithNode(node)
+        this.itBlocks.push(it)
+    }
+
+    isAnIt(node) {
+        return (
+            node.type === "ExpressionStatement" &&
+            node.expression.type === "CallExpression"
+        ) 
+        &&
+        (
+            node.expression.callee.name === "it" ||
+            node.expression.callee.name === "test"
+        ) 
+    }
+
+     isADescribe(node) {
+        return node.type === "ExpressionStatement" &&
+        node.expression.type === "CallExpression" &&
+        node.expression.callee.name === "describe"
+    }
+
+    findItBlocksInBody(root) {
+        for (var node in root.body) {
+            if (root.body.hasOwnProperty(node)) {
+                var element = root.body[node];
+                if (this.isADescribe(element)){
+                    if (element.expression.arguments.length == 2) {
+                        let newBody = element.expression.arguments[1].body
+                        this.findItBlocksInBody(newBody);
+                    }
+                }
+                if (this.isAnIt(element)) {
+                    this.foundItNode(element)
+                }        
+            }
+        }
+    }
+
+    generateAST(file: string): Promise<babylon.Node> {
+        return new Promise((resolve, reject) =>{
             var parentDir = path.resolve(process.cwd(), '..');
 
             fs.readFile(file, "utf8", (err, data) => {
                 if (err) { return reject(err.message) }
-                resolve(JSON.stringify(esprima.parse(data)))
+                resolve(babylon.parse(data, { sourceType:"module", plugins: ["jsx", "flow"] }))
             })  
         })
     }
--- test/test_file_parser.test.ts
+++ test/test_file_parser.test.ts
@@ -10,9 +10,46 @@ import Parser from '../src/test_file_parser'
 
 suite("File Parsing", () => {
 
-    test("Something 1", async () => {
+    test("For the simplest global case", async () => {
         let parser = new Parser()
-        const ast = await parser.run(__dirname + "/../../test/fixtures/jest-repl-test.jstest.js")
-        assert.equal(ast, {});
+        await parser.run(__dirname + "/../../test/fixtures/global_its.js")
+        assert.equal(parser.itBlocks.length, 2)
+        
+        let firstIt = parser.itBlocks[0]
+        assert.equal(firstIt.name, "works with old functions")
+        assert.notStrictEqual(firstIt.start, { line: 1, column: 0 })
+        assert.notStrictEqual(firstIt.end, { line: 3, column: 0 })
+
+        let secondIt = parser.itBlocks[1]
+        assert.equal(secondIt.name, "works with new functions")
+        assert.notStrictEqual(secondIt.start, { line: 5, column: 0 })
+        assert.notStrictEqual(secondIt.end, { line: 7, column: 0 })
     });
+
+    test("For its inside describes", async () => {
+        let parser = new Parser()
+        await parser.run(__dirname + "/../../test/fixtures/nested_its.js")
+        assert.equal(parser.itBlocks.length, 3)
+        
+        let firstIt = parser.itBlocks[0]
+        assert.equal(firstIt.name, "1")
+        assert.deepEqual(firstIt.start, { line: 2, column: 4 })
+        assert.deepEqual(firstIt.end, { line: 3, column: 6 })
+
+        let secondIt = parser.itBlocks[1]
+        assert.equal(secondIt.name, "2")
+        assert.deepEqual(secondIt.start, { line: 4, column: 4 })
+        assert.deepEqual(secondIt.end, { line: 5, column: 6 })
+
+        let thirdIt = parser.itBlocks[2]
+        assert.equal(thirdIt.name, "3")
+        assert.deepEqual(thirdIt.start, { line: 9, column: 4 })
+        assert.deepEqual(thirdIt.end, { line: 10, column: 6 })
+    });
+
+    test("For a danger test file (which has flow annotations)", async () => {
+        let parser = new Parser()
+        await parser.run(__dirname + "/../../test/fixtures/dangerjs/travis-ci.jstest.js")
+        assert.equal(parser.itBlocks.length, 7)
+    })
 });
