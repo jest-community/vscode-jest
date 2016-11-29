be4a9df2ef227fa48c53b2d10a3c3ae9a8f14a7f
Orta Therox <orta.therox@gmail.com>
2016-11-29 10:03:47 +0000

    vbump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-### 1.3.0
+### 1.3.0 - 1.3.1
 
 * Windows support - orta
 
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,13 +1,17 @@
 import {workspace} from 'vscode';
 import {platform} from 'os';
 
+/**
+ *  Handles getting the jest runner, handling the OS specific work too
+ * 
+ * @returns {string}
+ */
+
 export function pathToJest(): string {
   const jestSettings: any = workspace.getConfiguration("jest");
   const path: string = jestSettings.pathToJest;
 
   // For windows support, see https://github.com/orta/vscode-jest/issues/10
-  if (!path.includes(".bat") && platform() === "win32") {
-    return path + ".bat"; 
-  }
+  if (!path.includes(".cmd") && platform() === "win32") { return path + ".cmd";  }
   return path;
 }
