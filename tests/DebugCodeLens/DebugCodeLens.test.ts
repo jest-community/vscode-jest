b7cc55490383fc4189f0e1e813efbe580850324e
Egor Blinov <escaton@yandex-team.ru>
2018-09-19 16:28:49 +0300

    detach codeLensProviders from JestExt instance

--- tests/DebugCodeLens/DebugCodeLens.test.ts
+++ tests/DebugCodeLens/DebugCodeLens.test.ts
@@ -9,13 +9,18 @@ import { DebugCodeLens } from '../../src/DebugCodeLens/DebugCodeLens'
 import * as vscode from 'vscode'
 
 describe('DebugCodeLens', () => {
+  const document = {} as any
   const range = {} as any
   const fileName = 'file.js'
   const testName = 'should specify the test file name'
-  const sut = new DebugCodeLens(range, fileName, testName)
+  const sut = new DebugCodeLens(document, range, fileName, testName)
 
   it('should extend vscode.CodeLens', () => {
     expect(sut).toBeInstanceOf(vscode.CodeLens)
+  })
+
+  it('should specify the file document', () => {
+    expect(sut.document).toBe(document)
   })
 
   it('should specify the file name', () => {
