a9d2e68ed72a5f34db4daa70efc09101e8ea68b1
connectdotz <vsun@connectdotz.com>
2019-11-18 15:26:27 +0800

    update jest-editor-support versio and jest config

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,12 @@ module.exports = {
   testRegex: "tests/.*\\.ts$",
   automock: true,
   moduleFileExtensions: ["ts", "js", "json"],
-  unmockedModulePathPatterns: ["jest-editor-support/node_modules/"],
+  unmockedModulePathPatterns: [
+    "jest-editor-support/node_modules",
+    "color-convert",
+    "chalk",
+    "snapdragon",
+    "ansi-styles",
+    "core-js",
+  ],
 };
