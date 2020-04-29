a3864c11cdb2a0a44d57cdfaaf236307d5aa50a7
Michele Piccirillo <michele.piccirillo@gmail.com>
2020-04-29 14:47:34 +0200

    Add test for switching to `--watchAll` for "outside repository"

--- tests/Jest/index.test.ts
+++ tests/Jest/index.test.ts
@@ -6,6 +6,16 @@ describe('isWatchNotSupported', () => {
     const str = '\n--watch is not supported without git/hg, please use --watchAll \n'
     expect(isWatchNotSupported(str)).toBe(true)
   })
+  
+  it('returns true when matching an "out of the repository" message', () => {
+    const str = `
+      Determining test suites to run...
+
+      ● Test suite failed to run
+
+        fatal: ../packages/a-dependency-outside-the-submodule: '../packages/a-dependency-outside-the-submodule' is outside repository`
+    expect(isWatchNotSupported(str)).toBe(true)
+  })
 
   it('returns false otherwise', () => {
     expect(isWatchNotSupported()).toBe(false)
