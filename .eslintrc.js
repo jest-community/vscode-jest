603030ae820c9e86a18030464f2f8e82a188727d
connectdotz <vsun@connectdotz.com>
2020-05-24 19:04:50 -0400

    wrap prettier inside eslint and removed lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    "es6": true,
-    "node": true,
-    "jest/globals": true,
+    es6: true,
+    node: true,
+    'jest/globals': true,
   },
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-    'jest',
-  ],
+  plugins: ['@typescript-eslint', 'jest', 'prettier'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:jest/recommended',
-    'prettier',
     'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
-};
+  rules: {
+    'prettier/prettier': 'error',
+  },
+}
