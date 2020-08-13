3b64945002803e273f189b5cbfbc494d82096ff0
Tymoteusz Czech <mail@tymek.cz>
2020-08-13 04:01:39 +0200

    Status indicator in gutter (#599)

--- src/Coverage/Formatters/GutterFormatter/index.ts
+++ src/Coverage/Formatters/GutterFormatter/index.ts
@@ -1,73 +1,125 @@
 import { CoverageMapProvider } from '../../CoverageMapProvider';
 import { AbstractFormatter } from '../AbstractFormatter';
 import * as vscode from 'vscode';
+import { FileCoverage } from 'istanbul-lib-coverage';
+import { isValidLocation } from '../helpers';
 import { prepareIconFile } from '../../../helpers';
 import coverageGutterIcon from './coverage.svg';
-import { CoverageColors } from '../../CoverageOverlay';
+
+export interface CoverageLines {
+  covered: vscode.Range[];
+  partiallyCovered: vscode.Range[];
+  uncovered: vscode.Range[];
+}
 
 export class GutterFormatter extends AbstractFormatter {
-  readonly uncoveredLine: vscode.TextEditorDecorationType;
-  readonly partiallyCoveredLine: vscode.TextEditorDecorationType;
-  readonly coveredLine: vscode.TextEditorDecorationType;
+  private uncoveredLine: vscode.TextEditorDecorationType;
+  private partiallyCoveredLine: vscode.TextEditorDecorationType;
+  private coveredLine: vscode.TextEditorDecorationType;
 
-  constructor(
-    context: vscode.ExtensionContext,
-    coverageMapProvider: CoverageMapProvider,
-    colors?: CoverageColors
-  ) {
-    super(coverageMapProvider, colors);
+  constructor(context: vscode.ExtensionContext, coverageMapProvider: CoverageMapProvider) {
+    super(coverageMapProvider);
 
-    const coveredColor = this.getColorString('covered', 0.75);
-    const uncoveredColor = this.getColorString('uncovered', 0.75);
-    const partiallyCoveredColor = this.getColorString('partially-covered', 0.75);
     this.uncoveredLine = vscode.window.createTextEditorDecorationType({
-      overviewRulerColor: uncoveredColor,
+      isWholeLine: true,
+      backgroundColor: '',
+      overviewRulerColor: 'rgba(121, 31, 10, 0.75)',
       overviewRulerLane: vscode.OverviewRulerLane.Left,
-      gutterIconPath: this.iconUri(context, 'uncovered', coverageGutterIcon, uncoveredColor),
+      gutterIconPath: prepareIconFile(context, 'uncovered', coverageGutterIcon, '#791F0A'),
     });
 
     this.partiallyCoveredLine = vscode.window.createTextEditorDecorationType({
-      overviewRulerColor: partiallyCoveredColor,
+      backgroundColor: 'rgba(121, 86, 10, 0.75)',
+      overviewRulerColor: 'rgba(121, 86, 10, 0.75)',
       overviewRulerLane: vscode.OverviewRulerLane.Left,
-      gutterIconPath: this.iconUri(
-        context,
-        'partially-covered',
-        coverageGutterIcon,
-        partiallyCoveredColor
-      ),
+      gutterIconPath: prepareIconFile(context, 'partially-covered', coverageGutterIcon, '#79560A'),
     });
 
     this.coveredLine = vscode.window.createTextEditorDecorationType({
-      gutterIconPath: this.iconUri(context, 'covered', coverageGutterIcon, coveredColor),
+      isWholeLine: true,
+      backgroundColor: '',
+      overviewRulerColor: '',
+      overviewRulerLane: vscode.OverviewRulerLane.Left,
+      gutterIconPath: prepareIconFile(context, 'covered', coverageGutterIcon, '#2D790A'),
     });
   }
-  // convert iconPath to uri to prevent render cache icon by fileName alone
-  // even after file content has changed such as color changed
-  private iconUri(
-    context: vscode.ExtensionContext,
-    iconName: string,
-    source: string,
-    color: string
-  ): vscode.Uri {
-    const iconPath = prepareIconFile(context, iconName, source, color);
-    return vscode.Uri.file(iconPath).with({ query: `color=${color}` });
-  }
+
   format(editor: vscode.TextEditor): void {
-    const coverageRanges = this.lineCoverageRanges(editor, () => 'covered');
-    editor.setDecorations(this.uncoveredLine, coverageRanges['uncovered'] ?? []);
-    editor.setDecorations(this.partiallyCoveredLine, coverageRanges['partially-covered'] ?? []);
-    editor.setDecorations(this.coveredLine, coverageRanges['covered'] ?? []);
+    const fileCoverage = this.coverageMapProvider.getFileCoverage(editor.document.fileName);
+    if (!fileCoverage) {
+      return;
+    }
+
+    const coverageFormatting = this.computeFormatting(editor, fileCoverage);
+
+    editor.setDecorations(this.coveredLine, coverageFormatting.covered);
+    editor.setDecorations(this.uncoveredLine, coverageFormatting.uncovered);
+    editor.setDecorations(this.partiallyCoveredLine, coverageFormatting.partiallyCovered);
+  }
+
+  computeFormatting(editor: vscode.TextEditor, fileCoverage: FileCoverage): CoverageLines {
+    const coverageFormatting: CoverageLines = {
+      covered: [],
+      partiallyCovered: [],
+      uncovered: [],
+    };
+
+    const uncoveredLines = fileCoverage.getUncoveredLines();
+
+    for (let line = 1; line <= editor.document.lineCount; line++) {
+      const zeroBasedLineNumber = line - 1;
+      if (uncoveredLines.indexOf(line) >= 0) {
+        coverageFormatting.uncovered.push(
+          new vscode.Range(zeroBasedLineNumber, 0, zeroBasedLineNumber, 0)
+        );
+      } else {
+        coverageFormatting.covered.push(
+          new vscode.Range(zeroBasedLineNumber, 0, zeroBasedLineNumber, 0)
+        );
+      }
+    }
+
+    Object.keys(fileCoverage.b).forEach((branchIndex) => {
+      fileCoverage.b[branchIndex].forEach((hitCount, locationIndex) => {
+        if (hitCount > 0) {
+          return;
+        }
+
+        const branch = fileCoverage.branchMap[branchIndex].locations[locationIndex];
+        if (!isValidLocation(branch)) {
+          return;
+        }
+
+        const partialLineRange = new vscode.Range(
+          branch.start.line - 1,
+          0,
+          branch.start.line - 1,
+          0
+        );
+        coverageFormatting.covered = coverageFormatting.covered.filter(
+          (range) => !range.isEqual(partialLineRange)
+        );
+        coverageFormatting.uncovered = coverageFormatting.uncovered.filter(
+          (range) => !range.isEqual(partialLineRange)
+        );
+
+        coverageFormatting.partiallyCovered.push(
+          new vscode.Range(
+            branch.start.line - 1,
+            branch.start.column,
+            branch.end.line - 1,
+            branch.end.column
+          )
+        );
+      });
+    });
+
+    return coverageFormatting;
   }
 
   clear(editor: vscode.TextEditor): void {
     editor.setDecorations(this.coveredLine, []);
     editor.setDecorations(this.partiallyCoveredLine, []);
     editor.setDecorations(this.uncoveredLine, []);
-  }
-
-  dispose(): void {
-    this.coveredLine.dispose();
-    this.partiallyCoveredLine.dispose();
-    this.uncoveredLine.dispose();
   }
 }
