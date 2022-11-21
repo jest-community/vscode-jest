9942deeaaf1aa9d4cc3fb83149765a40f66bee79
ConnectDotz <vsun@connectdotz.com>
2022-11-20 22:30:27 -0500

    Retire debug codelens (#950)
    * removed debugCodeLens and settings
    
    * clean up more snapshot and debug codeLens settings

--- tests/extensionManager.test.ts
+++ tests/extensionManager.test.ts
@@ -7,7 +7,6 @@ import {
   ExtensionManager,
   getExtensionWindowSettings,
 } from '../src/extensionManager';
-import { DebugCodeLensProvider, TestState } from '../src/DebugCodeLens';
 import { readFileSync } from 'fs';
 import { PluginWindowSettings } from '../src/Settings';
 import { extensionName } from '../src/appGlobals';
@@ -129,7 +128,6 @@ describe('ExtensionManager', () => {
     it('should created the components shared across of all workspaces', () => {
       new ExtensionManager(context);
       expect(DebugConfigurationProvider).toHaveBeenCalled();
-      expect(DebugCodeLensProvider).toHaveBeenCalled();
       expect(CoverageCodeLensProvider).toHaveBeenCalled();
     });
   });
@@ -170,27 +168,10 @@ describe('ExtensionManager', () => {
     describe('applySettings()', () => {
       it('should save settings to instance', () => {
         const newSettings: PluginWindowSettings = {
-          debugCodeLens: {
-            enabled: true,
-            showWhenTestStateIn: [],
-          },
           disabledWorkspaceFolders: [],
         };
         extensionManager.applySettings(newSettings);
         expect((extensionManager as any).commonPluginSettings).toEqual(newSettings);
-      });
-      it('should update debugCodeLensProvider instance', () => {
-        const newSettings: PluginWindowSettings = {
-          debugCodeLens: {
-            enabled: true,
-            showWhenTestStateIn: [TestState.Fail],
-          },
-          disabledWorkspaceFolders: ['workspaceFolder1'],
-        };
-        extensionManager.applySettings(newSettings);
-        expect((extensionManager as any).debugCodeLensProvider.showWhenTestStateIn).toEqual(
-          newSettings.debugCodeLens.showWhenTestStateIn
-        );
       });
 
       it('should respect disabledWorkspaceFolders', () => {
@@ -198,10 +179,6 @@ describe('ExtensionManager', () => {
         expect(extensionManager.getByName('workspaceFolder1')).toBeDefined();
         expect(extensionManager.getByName('workspaceFolder2')).toBeDefined();
         const newSettings: PluginWindowSettings = {
-          debugCodeLens: {
-            enabled: true,
-            showWhenTestStateIn: [],
-          },
           disabledWorkspaceFolders: ['workspaceFolder1'],
         };
         extensionManager.applySettings(newSettings);
@@ -212,10 +189,6 @@ describe('ExtensionManager', () => {
         expect(extensionManager.getByName('workspaceFolder1')).not.toBeUndefined();
 
         const newSettings: PluginWindowSettings = {
-          debugCodeLens: {
-            enabled: true,
-            showWhenTestStateIn: [],
-          },
           disabledWorkspaceFolders: ['workspaceFolder1'],
         };
         extensionManager.applySettings(newSettings);
@@ -600,11 +573,6 @@ describe('ExtensionManager', () => {
     describe('getExtensionWindowSettings()', () => {
       it('should return the extension window configuration', async () => {
         expect(getExtensionWindowSettings()).toEqual({
-          debugCodeLens: {
-            enabled: true,
-            showWhenTestStateIn: [TestState.Fail, TestState.Unknown],
-          },
-          enableSnapshotPreviews: true,
           disabledWorkspaceFolders: [],
         });
       });
