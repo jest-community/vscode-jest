de55a55582518a39b444bfae131f5ea25a4d7b11
connectdotz <vsun@connectdotz.com>
2023-01-15 16:43:34 -0500

    prepare for v5.2.0 release

--- release-notes/release-notes.md
+++ release-notes/release-notes.md
@@ -1,3 +1,3 @@
 # vscode-jest Release Notes
-- [v5](release-note-v5.md)
+- [v5](release-note-v5.x.md)
 - [v4](release-note-v4.md)
--- src/extensionManager.ts
+++ src/extensionManager.ts
@@ -463,7 +463,8 @@ export class ExtensionManager {
 
 const ReleaseNoteBase = 'https://github.com/jest-community/vscode-jest/blob/master/release-notes';
 const ReleaseNotes: Record<string, string> = {
-  '5.1.0': `${ReleaseNoteBase}/release-note-v5.1.md#v510`,
+  '5.2.0': `${ReleaseNoteBase}/release-note-v5.x.md#v520-pre-release`,
+  '5.1.0': `${ReleaseNoteBase}/release-note-v5.x.md#v510`,
   '5.0.4': `${ReleaseNoteBase}/release-note-v5.md#v50-pre-release-roll-up`,
   '5.0.3': `${ReleaseNoteBase}/release-note-v5.md#v50-pre-release-roll-up`,
   '5.0.2': `${ReleaseNoteBase}/release-note-v5.md#v50-pre-release-roll-up`,
