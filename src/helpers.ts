72dc316f3085fea91720068d8346f92975d5229b
ConnectDotz <vsun@connectdotz.com>
2025-04-03 13:55:17 -0700

    fixed command line parsing crashed debugger session (#1224)

--- src/helpers.ts
+++ src/helpers.ts
@@ -314,7 +314,7 @@ export const parseCmdLine = (cmdLine: string): string[] => {
   const parts = cmdLine.match(cmdSplitRegex) || [];
   // clean up command
   if (parts.length > 0 && parts[0]) {
-    parts[0] = removeSurroundingQuote(normalize(parts[0]));
+    parts[0] = normalize(removeSurroundingQuote(parts[0]));
   }
   return parts;
 };
